5529807cf4e52d2a847a63608494a3733c8f8b66
smori <shinichiro.mori.1983@gmail.com>
2020-05-18 23:48:17 +0900

    Repair renderer process codes

---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div>
+      <input v-model="directory" class="directory" type="text" placeholder="directory">
+      <button v-on:click="selectDirectory" class="" type="button">Browse</button>
+    </div>
+    <div>
+      <button v-on:click="startServer" type="button">Start</button>
+      <button v-on:click="stopServer" type="button">Stop</button>
+    </div>
+    <div>
+      <div v-html="log" class="log"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+const fs = require('fs');
+const { ipcRenderer } = require('electron'); // eslint-disable-line
+const { dialog } = require('electron').remote; // eslint-disable-line
+
+/**
+ * @param {string} directory
+ */
+const validate = (directory) => {
+  try {
+    return directory.length > 0 && fs.statSync(directory).isDirectory();
+  } catch (e) {
+    return false;
+  }
+};
+
+export default {
+  name: 'home',
+  data: () => ({
+    directory: '',
+    log: '',
+  }),
+  methods: {
+    selectDirectory() {
+      dialog.showOpenDialog({
+        properties: ['openDirectory'],
+      }, (path) => {
+        [this.directory] = path;
+      });
+    },
+    startServer() {
+      if (validate(this.directory)) {
+        ipcRenderer.send('server-start', {
+          directory: this.directory,
+        });
+      }
+    },
+    stopServer() {
+      ipcRenderer.send('server-stop');
+    },
+  },
+  mounted() {
+    // Temporary implementation
+    ipcRenderer.on('server-started', () => {
+      this.log += 'started<br>';
+    });
+
+    ipcRenderer.on('server-stopped', () => {
+      this.log += 'stopped<br>';
+    });
+  },
+};
+</script>
+
+<style>
+.directory {
+  width: 300px;
+}
+</style>
